083679067d2502e186702a80f40d6da0d8c05bd4
Nkwesi Ditrich <ditrichnkwesi@gmail.com>
2025-09-08 14:32:56 +0100

    fixed navigation

--- resources/views/frontend/components/home/footer.blade.php
+++ resources/views/frontend/components/home/footer.blade.php
@@ -1,11 +1,8 @@
 <!-- Begin Footer
 ================================================== -->
 <div class="footer">
-    <p class="pull-left">
-            Copyright &copy; {{ date('Y') }} Event Managment
-    </p>
     <p class="pull-right">
-            Developed by <a target="_blank" href="https://www.coderstream.com">coderstream.com</a>
+            Internship project By Nkwesi Ditrich
     </p>
     <div class="clearfix">
     </div>
--- resources/views/frontend/pages/post-page.blade.php
+++ resources/views/frontend/pages/post-page.blade.php
@@ -2,7 +2,6 @@
 @section('content')
     @include('frontend.components.post.article')
     @include('frontend.components.post.raleted')
-    @include('frontend.components.post.promote')
     @include('frontend.components.post.alert')
     @include('frontend.components.post.modal')
     @include('frontend.components.home.footer')
